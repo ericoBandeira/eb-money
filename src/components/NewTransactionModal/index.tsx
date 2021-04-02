a1a675e408375ca82bd61316d43db8a05c4d9ce9
ericoBandeira <ericobandeira@hotmail.com>
2021-04-02 00:20:53 -0300

    adicionando base do modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal'
+import { Container } from './styles'
 
 interface NewTransactionModalProps{
     isOpen: boolean;
@@ -13,8 +14,25 @@ export function NewTransactionModal({isOpen, onResquestClose}:NewTransactionModa
         <Modal
           isOpen={isOpen}
           onRequestClose={onResquestClose}
+          overlayClassName="react-modal-overlay"
+          className="react-modal-content"
         >
-          <h2>cadastrar transação</h2>
+          <Container>
+              <h2>Cadastrar transações</h2>
+              <input
+                placeholder="Título"
+              />
+              <input
+                type = "number"
+                placeholder="Valor"
+              />
+              <input
+                placeholder="Categoria"
+              />
+              <button type="submit">
+                Cadastrar
+              </button>
+          </Container>
       </Modal>
     );
 }
