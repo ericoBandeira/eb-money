0074091f2811d433c530500fcc6a14f1dbe6d49a
ericoBandeira <ericobandeira@hotmail.com>
2021-04-10 21:39:33 -0300

    Adicionando logica no sumário

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -9,6 +9,31 @@ import { useContext } from "react";
 export function Summary(){
 
     const {transactions} = useContext(TransactionContext);
+
+    // const totalDeposits = transactions.reduce((acc,transaction)=>{
+    //     if(transaction.type === 'deposit'){
+    //         return acc + transaction.amount;
+    //     }
+
+    //     return acc;
+    // },0)
+
+    const summary = transactions.reduce((acc,transaction)=>{
+        if(transaction.type === 'deposit'){
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+        }else{
+            acc.withdraws += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+
+        return acc;
+    },{
+        deposits:0,
+        withdraws: 0,
+        total: 0
+    })
+
     return(
 
         <Container>
@@ -17,21 +42,36 @@ export function Summary(){
                     <p>Entradas</p>
                     <img src={incomeImg}alt="Entradas"/>
                 </header>
-                <strong>R$1000,00</strong>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR',{
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.deposits)}
+                    </strong>
             </div>
             <div>
                 <header>
                     <p>Saida</p>
                     <img src={outcomeImg}alt="Saidas"/>
                 </header>
-                <strong> - R$500,00</strong>
+                <strong> 
+                    - {new Intl.NumberFormat('pt-BR',{
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.withdraws)}
+                </strong>
             </div>
             <div className="highlight-background">
                 <header>
                     <p>Total</p>
                     <img src={totalImg}alt="Total"/>
                 </header>
-                <strong>R$500,00</strong>
+                <strong>   
+                    {new Intl.NumberFormat('pt-BR',{
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.total)}
+                </strong>
             </div>
         </Container>
     );
